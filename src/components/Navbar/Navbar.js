8627c8c19bfc4872935deb56d6862efd3f15f458
Recep Terzi <recepterzi-67@hotmail.com>
2022-04-17 16:55:39 +0300

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { MenuItems } from "./MenuItems"
 import './Navbar.css'
+import {Link} from "react-router-dom"
 class Navbar extends Component {
     state = { clicked: false }
 
     handleClick = () => {
         this.setState({ clicked: !this.state.clicked })
     }
+
+    
 
     render() {
         return(
@@ -15,13 +18,13 @@ class Navbar extends Component {
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fa fa-times' : 'fa fa-bars'}></i>
                 </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'} onClick={this.handleClick}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                                <a className={item.cName} href={item.url} style={{color:"white"}}>
+                                <Link className={item.cName} to={item.url} style={{color:"white"}} >
                                 {item.title}
-                                </a>
+                                </Link>
                             </li>
                         )
                     })}
