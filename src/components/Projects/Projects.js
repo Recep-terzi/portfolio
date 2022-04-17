fdbedd7d93b3293b5709d0796842c6d830512b0d
Recep Terzi <recepterzi-67@hotmail.com>
2022-04-17 17:38:26 +0300

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,10 @@
 import React from "react";
 import items from "../data";
 import "./Projects.css";
-
 const Projects = () => {
   return (
     <>
-      <div className="container mt-5 ">
+      <div className="container mt-5 " >
         <div className="row ">
           {items.map((projectItem) => {
             const { id, title, img, description, url } = projectItem;
@@ -28,8 +27,9 @@ const Projects = () => {
                     </p>
                    
                   </div>
-                  <button class="btn btn-outline-danger text-white a" >
-                      <a href={url} className="text-decoration-none text-white bg-none rounded " target="_blank" >Show Project</a>
+                  
+                  <button class="btn btn-outline-danger text-white a"  >
+                      <a href={url} className="text-decoration-none text-white bg-none rounded " target={"_blank"} rel="noopener noreferrer">Show Project</a>
                   </button>
                 </div>
               </div>
