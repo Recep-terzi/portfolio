591ea53ff5cb31346efb834d789c27e3332b859f
Recep Terzi <recepterzi-67@hotmail.com>
2022-04-17 17:54:42 +0300

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,12 +4,12 @@ import "./Projects.css";
 const Projects = () => {
   return (
     <>
-      <div className="container mt-5 " >
+      <div className="container mt-5 ">
         <div className="row ">
           {items.map((projectItem) => {
             const { id, title, img, description, url } = projectItem;
             return (
-              <div className="col-sm-4 text-white mb-3 " key = {id}>
+              <div className="col-sm-4 text-white mb-3 " key={id}>
                 <div
                   class="card rounded border-danger bg-dark "
                   style={{ width: "25rem", height: "100%" }}
@@ -18,19 +18,23 @@ const Projects = () => {
                     src={img}
                     class="card-img-top"
                     alt={title}
-                    style = {{width:"100%"}}
+                    style={{ width: "100%" }}
                   />
                   <div class="card-body">
                     <h5 class="card-title">{title}</h5>
-                    <p class="card-text">
-                      {description}
-                    </p>
-                   
+                    <p class="card-text">{description}</p>
                   </div>
-                  
-                  <button class="btn btn-outline-danger text-white a"  >
-                      <a href={url} className=" text-white bg-none rounded "target="_blank" rel="noopener noreferrer" >Show Project</a>
-                  </button>
+
+                  <a
+                    href={url}
+                    className=" btn btn-outline-danger text-white  rounded a"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button class=" text-white  btn btn-outline-danger">
+                      Show Project
+                    </button>
+                  </a>
                 </div>
               </div>
             );
