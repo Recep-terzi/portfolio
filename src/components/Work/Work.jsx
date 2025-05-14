8743b42efe3579003f35283552848f2f32fe7c41
Recep Terzi <71466957+Recep-terzi@users.noreply.github.com>
2025-05-14 16:08:52 +0300

    feat(fix): upgrade

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -6,11 +6,31 @@ import tusecApp from '../../assets/tusecapp.png'
 import swapApp from '../../assets/swapapp.png'
 import difoApp from '../../assets/difo.png'
 import memeApp from '../../assets/memelaunchpad.png'
+import linguetaApp from '../../assets/lingueta.png'
 
 const Work = () => {
     const myWork = [
         {
             id: 1,
+            projectImage: linguetaApp,
+            projectTitle: "Lingueta",
+            link:"https://lingueta.com",
+            projectInfo: "I coded this project, which offers translation services in a professional and fun way, responsively in a user-friendly way using current technologies such as Next.js and Typescript.",
+            projectMaterial: [
+                "HTML",
+                "CSS",
+                "Javascript",
+                "React",
+                "Next.js",
+                "Typescript",
+                "Figma",
+                "Firebase",
+                "API",
+                "Web Socket"
+            ]
+        },
+        {
+            id: 2,
             projectImage: difoApp,
             projectTitle: "Difo Launchpad",
             link:"https://difolaunchpad.com",
@@ -29,7 +49,7 @@ const Work = () => {
             ]
         },
         {
-            id: 2,
+            id: 3,
             projectImage: memeApp,
             projectTitle: "Meme Launchpad",
             link:"https://memelaunchpad.io",
@@ -48,7 +68,7 @@ const Work = () => {
             ]
         },
         {
-            id: 2,
+            id: 4,
             projectImage: swapApp,
             projectTitle: "Swap App",
             projectInfo: "Responsively tuned swap website using React and redux. Authentication, Firestore Database, Firebase Storage are used. Reduxjs/toolkit is used for redux. There are also operations such as adding products, deleting products, editing products, sending exchange offers, and purchasing. I think it's a detailed react-redux project.",
@@ -63,7 +83,7 @@ const Work = () => {
             ]
         },
         {
-            id: 3,
+            id: 5,
             projectImage: tusecApp,
             projectTitle: "Tusec Website",
             projectInfo: "Hello. In this project, I am here with the website I made for TUSEC, the cyber security cluster of our school, in line with the needs. I think it is a detailed and beautiful website. It is a responsive website. Firebase auth, Firestore database... are used.",
@@ -78,7 +98,7 @@ const Work = () => {
             ]
         },
         {
-            id: 4,
+            id: 6,
             projectImage: dietApp,
             projectTitle: "Diet App",
             projectInfo: "A website that dietitians and users can use dynamically. Firebase authentication and firebase database were used on the backend. It was made as a university course project.",
